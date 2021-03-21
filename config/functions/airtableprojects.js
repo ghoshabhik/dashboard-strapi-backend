f6e4546a75553901eb7b31a6e8a34fc9dd27f0f9
abhik <avik5324@gmail.com>
2021-03-22 02:53:48 +0530

    added airtable apis

---
+++ config/functions/airtableprojects.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+module.exports = async () => {
+    const AIRTABLE_API_TOKEN = process.env.AIRTABLE_API_TOKEN;
+    const config = {
+        headers: { Authorization: `Bearer ${AIRTABLE_API_TOKEN}` }
+    };
+    const { data } = await axios.get('https://api.airtable.com/v0/appnJyGDL7ilErfRQ/projects', config);
+
+    console.log(data.records);
+    return data.records;
+};
--- config/middleware.js
+++ config/middleware.js
@@ -1,7 +1,7 @@
 module.exports = {
     settings: {
       cors: {
-        origin: ['http://localhost:3000', 'https://example.com', 'https://dashboard-strapi-backend.herokuapp.com'],
+        origin: ['http://localhost:3000', 'https://example.com', 'https://dashboard-strapi-backend.herokuapp.com', 'http://localhost:1337'],
       },
     },
   };
